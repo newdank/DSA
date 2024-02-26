8741692ab115d1fff734a3f53ba4d02c5f3cb9fa
newdank <nikhil1vasan@gmail.com>
2024-02-27 00:09:53 +0530

    dsa

--- dsa_proj/src/SetMismatch.java
+++ dsa_proj/src/SetMismatch.java
@@ -2,9 +2,13 @@ import java.util.Arrays;
 
 public class SetMismatch {
     public static void main(String[] args) {
-        int[] arr = {3,7,1,4,2,7,5};
-        int[] arr1 = sort(arr);
-        System.out.println(Arrays.toString(arr1));
+        int[] arr = {7,8,9,11,12};
+//        int[] arr1 = sort(arr);
+        int arrlo = sortfnd(arr);
+        System.out.println(arrlo);
+//        System.out.println(Arrays.toString(arr1));
+
+
     }
 
     public static int[] sort(int[] arr){
@@ -25,6 +29,26 @@ public class SetMismatch {
             }
         }
         return new int[] {-1,-1};
+    }
+
+    public static int sortfnd(int[] arr){
+        int i = 0;
+        while(i<arr.length){
+            int correct  = arr[i]-1;
+            if(arr[i]>0 && arr[i]<arr.length && arr[correct]!=arr[i]){
+                swap(arr,correct,i);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int index=0; index<arr.length; index++){
+            if(arr[index]!=index+1){
+                return index+1;
+            }
+        }
+        return arr.length+1;
     }
 
     public static void swap (int[] arr, int first,int second) {
